40d9c5816d5f5f3bdc3084f1185dc44f81e083c6
Bruce Ruffin <bruffin1221@gmail.com>
2019-10-15 13:15:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,16 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-
+  attr_accessor :brand
 
   BRANDS=[]
 
   def initialize(brand)
     @brand = brand
-    BRANDS<<@brand
+  end
+
+  def brand
+    @brand=brand
+    BRANDS<<brand
   end
 
   def cobble
